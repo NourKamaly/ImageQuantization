6b807374859b631dadd40599316f80f67d429d0f
Nour Kamaly <nourkamaly26@gmail.com>
2022-05-11 23:18:43 +0200

    Initial priority queue class

---
+++ Template/[2] Image Quantization/Image Quantization Startup Code/[TEMPLATE] ImageQuantization/ImageQuantization/PriorityQueue.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageQuantization
+{
+    internal class PriorityQueue
+    {
+        public struct RGBPixel
+        {
+            public byte red, green, blue;
+        }
+
+         
+    }
+}
